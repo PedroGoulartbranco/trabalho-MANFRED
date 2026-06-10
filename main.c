28ee0a576ce34fd951e4475156932f6cc22c792a
Vitor R <vitinho.marcelino2017@gmail.com>
2026-06-09 21:06:44 -0300

    função limpar tela e começo da função de incluir

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void limpar_termial() {
+    if (system("cls") == -1) {
+        system("clear");
+    }
+}
+
 void mostrar_menu() {
     printf("=====================================\n");
     printf("                Menu                 \n");
     printf("=====================================\n");
-    printf("[1]- Incluir contato\n[2] Listar Contatos\n[3] Consultar contato pelo nome\n[4] Excluir contato\n[5] Sair\n");
+    printf("[1] Incluir contato\n[2] Listar Contatos\n[3] Consultar contato pelo nome\n[4] Excluir contato\n[5] Sair\n");
+}
+
+void opcao_incluir() {
+    limpar_termial();
+
+    printf("=====================================\n");
+    printf("             Incluir                 \n");
+    printf("=====================================\n");
+    
 }
 
 int main() {
@@ -19,7 +34,7 @@ int main() {
 
         switch (opcao) {
         case 1:
-            printf("Fazer para incluir");
+            opcao_incluir();
             break;
         case 2:
             printf("Fazer para Listar");
